a4048ed38ec7455b538ac84d00262a73a35d597d
amb <mikaelh1651@gmail.com>
2023-02-28 16:29:29 +0900

    feat(schedule_provider): schedule provider 정의

---
+++ lib/provider/schedule_provider.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calendar_scheduler/model/schedule_model.dart';
+import 'package:flutter_calendar_scheduler/repository/schedule_repository.dart';
+
+class ScheduleProvider extends ChangeNotifier {
+  final ScheduleRepository repository;
+
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  Map<DateTime, List<ScheduleModel>> cache = {};
+
+  ScheduleProvider({required this.repository}) : super() {
+    getSchedules(date: selectedDate);
+  }
+
+  void getSchedules({required DateTime date}) async {
+    final response = await repository.getSchedules(date: date);
+    cache.update(date, (value) => response, ifAbsent: () => response);
+    notifyListeners();
+  }
+
+  void createSchedule({
+    required ScheduleModel schedule,
+  }) async {
+    final targetDate = schedule.date;
+    final savedSchedule = await repository.createSchedule(schedule: schedule);
+    cache.update(
+      targetDate,
+      (value) => [
+        ...value,
+        schedule.copyWith(
+          id: savedSchedule,
+        ),
+      ]..sort(
+          (a, b) => a.startTime.compareTo(
+            b.startTime,
+          ),
+        ),
+      ifAbsent: () => [schedule],
+    );
+    notifyListeners();
+  }
+
+  void deleteSchedule({
+    required DateTime date,
+    required String id,
+  }) async {
+    final response = await repository.deleteSchedule(id: id);
+    cache.update(
+      date,
+      (value) => value.where((element) => element.id != id).toList(),
+      ifAbsent: () => [],
+    );
+    notifyListeners();
+  }
+
+  void changeSelectedDate({
+    required DateTime date,
+  }) {
+    selectedDate = date;
+    notifyListeners();
+  }
+}

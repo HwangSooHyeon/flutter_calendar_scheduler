5dbf38d0186aa88e3b68c99c4e9359d809769177
amb <mikaelh1651@gmail.com>
2023-02-28 18:00:51 +0900

    feat: streambuilder 및 상태 관리 전체 provider로 전환

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_calendar_scheduler/component/custom_text_field.dart';
 import 'package:flutter_calendar_scheduler/const/colors.dart';
 import 'package:drift/drift.dart' hide Column;
+import 'package:flutter_calendar_scheduler/model/schedule_model.dart';
+import 'package:flutter_calendar_scheduler/provider/schedule_provider.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_calendar_scheduler/database/drift_database.dart';
+import 'package:provider/provider.dart';
 
 class ScheduleBottomSheet extends StatefulWidget {
   final DateTime selectedDate;
@@ -82,7 +85,7 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: onSavePressed,
+                    onPressed: () => onSavePressed(context),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: PRIMARY_COLOR,
                     ),
@@ -99,18 +102,19 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
     );
   }
 
-  void onSavePressed() async {
+  void onSavePressed(BuildContext context) async {
     if (formKey.currentState!.validate()) {
       formKey.currentState!.save();
 
-      await GetIt.I<LocalDatabase>().createSchedule(
-        SchedulesCompanion(
-          startTime: Value(startTime!),
-          endTime: Value(endTime!),
-          content: Value(content!),
-          date: Value(widget.selectedDate),
-        ),
-      );
+      context.read<ScheduleProvider>().createSchedule(
+            schedule: ScheduleModel(
+              id: 'new_model',
+              content: content!,
+              date: widget.selectedDate,
+              startTime: startTime!,
+              endTime: endTime!,
+            ),
+          );
 
       Navigator.of(context).pop();
     }

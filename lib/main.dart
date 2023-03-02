07ce84c89ce17862ea53f79db0747dd7923aa8c5
amb <mikaelh1651@gmail.com>
2023-03-02 17:33:59 +0900

    feat: firebase 사용을 위한 설정 및 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_scheduler/database/drift_database.dart';
+import 'package:flutter_calendar_scheduler/firebase_options.dart';
 import 'package:flutter_calendar_scheduler/provider/schedule_provider.dart';
 import 'package:flutter_calendar_scheduler/repository/schedule_repository.dart';
 import 'package:flutter_calendar_scheduler/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await initializeDateFormatting();
 
   final database = LocalDatabase();
 
-  GetIt.I.registerSingleton<LocalDatabase>(database);
-
   final repository = ScheduleRepository();
   final scheduleProvider = ScheduleProvider(repository: repository);
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);
 
   runApp(
     ChangeNotifierProvider(

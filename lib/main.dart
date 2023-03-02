1ee0bcb4d2baadedcd2e68db84327574d0b304ab
amb <mikaelh1651@gmail.com>
2023-03-02 18:37:35 +0900

    feat: 광고 적용을 위한 초기 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_scheduler/database/drift_database.dart';
 import 'package:flutter_calendar_scheduler/firebase_options.dart';
-import 'package:flutter_calendar_scheduler/provider/schedule_provider.dart';
-import 'package:flutter_calendar_scheduler/repository/schedule_repository.dart';
 import 'package:flutter_calendar_scheduler/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:get_it/get_it.dart';
-import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  MobileAds.instance.initialize();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
